bfc05f14e694f4bae36d87456fb0e6606b315c12
Gabriel Gonzales <gabrielgonzalesroca@gmail.com>
2024-06-26 11:27:43 -0500

    initial commit

---
+++ src/main/java/pe/glowchemicaleirl/GlowchemicaleirlApplication.java
@@ -0,0 +1,13 @@
+package pe.glowchemicaleirl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowchemicaleirlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlowchemicaleirlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/glowchemicaleirl/GlowchemicaleirlApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.glowchemicaleirl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowchemicaleirlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
